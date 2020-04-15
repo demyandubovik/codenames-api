7ff08402c916d5b3f952089f794992be71211327
Demyan Dubovik <demyandubovik@gmail.com>
2020-04-15 15:47:02 +0300

    Dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+require('dotenv').config()
+
 import Koa from 'koa'
+import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser'
+import { router } from 'routes'
 
 const app = new Koa()
+
+app.use(bodyParser())
+app.use(cors({ origin: '*' }))
+
+app.use(router.routes())
 
 app.use(async ctx => {
   ctx.body = 'ts!'
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
